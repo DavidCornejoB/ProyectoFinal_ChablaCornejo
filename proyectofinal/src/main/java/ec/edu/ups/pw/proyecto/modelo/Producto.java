126dec3ea0add50c436f8736b20753c1bd3e4253
Elwilliko <52544334+Elwilliko@users.noreply.github.com>
2021-08-01 21:18:59 -0500

    Dependencia Fk

--- proyectofinal/src/main/java/ec/edu/ups/pw/proyecto/modelo/Producto.java
+++ proyectofinal/src/main/java/ec/edu/ups/pw/proyecto/modelo/Producto.java
@@ -1,8 +1,12 @@
 package ec.edu.ups.pw.proyecto.modelo;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Producto {
@@ -13,8 +17,85 @@ public class Producto {
 	@Column(length = 20, name = "pro_nombre")
 	private String nombre;
 	
+	@ManyToOne
+	private CarritoDetalle carritoDetalle;
+	
 	private Double valorUnitario;
 	private int stock;
+	
+	@ManyToOne
+	private Categoria categoria;
+	
+	@ManyToOne
+	private Bodega bodega;
+	
+	@OneToMany
+	private List<Detalle_Factura> listDetalleFactura;
+	
+	@ManyToOne
+	private ReporteIngresos reporteIngresos;
+	
+	
+	/**
+	 * @return the listDetalleFactura
+	 */
+	public List<Detalle_Factura> getListDetalleFactura() {
+		return listDetalleFactura;
+	}
+	/**
+	 * @param listDetalleFactura the listDetalleFactura to set
+	 */
+	public void setListDetalleFactura(List<Detalle_Factura> listDetalleFactura) {
+		this.listDetalleFactura = listDetalleFactura;
+	}
+	/**
+	 * @return the reporteIngresos
+	 */
+	public ReporteIngresos getReporteIngresos() {
+		return reporteIngresos;
+	}
+	/**
+	 * @param reporteIngresos the reporteIngresos to set
+	 */
+	public void setReporteIngresos(ReporteIngresos reporteIngresos) {
+		this.reporteIngresos = reporteIngresos;
+	}
+	/**
+	 * @return the bodega
+	 */
+	public Bodega getBodega() {
+		return bodega;
+	}
+	/**
+	 * @param bodega the bodega to set
+	 */
+	public void setBodega(Bodega bodega) {
+		this.bodega = bodega;
+	}
+	/**
+	 * @return the categoria
+	 */
+	public Categoria getCategoria() {
+		return categoria;
+	}
+	/**
+	 * @param categoria the categoria to set
+	 */
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	/**
+	 * @return the carritoDetalle
+	 */
+	public CarritoDetalle getCarritoDetalle() {
+		return carritoDetalle;
+	}
+	/**
+	 * @param carritoDetalle the carritoDetalle to set
+	 */
+	public void setCarritoDetalle(CarritoDetalle carritoDetalle) {
+		this.carritoDetalle = carritoDetalle;
+	}
 	/**
 	 * @return the codigo
 	 */
--- proyectofinal/src/main/java/ec/edu/ups/pw/proyecto/modelo/ReporteEgresos.java
+++ proyectofinal/src/main/java/ec/edu/ups/pw/proyecto/modelo/ReporteEgresos.java
@@ -1,13 +1,35 @@
 package ec.edu.ups.pw.proyecto.modelo;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class ReporteEgresos {
 
 	@Id
 	private int codigo;
+	
+	@OneToMany
+	private List<Detalle_Factura> listDeatlleFactura;
+	
+	
+
+	/**
+	 * @return the listDeatlleFactura
+	 */
+	public List<Detalle_Factura> getListDeatlleFactura() {
+		return listDeatlleFactura;
+	}
+
+	/**
+	 * @param listDeatlleFactura the listDeatlleFactura to set
+	 */
+	public void setListDeatlleFactura(List<Detalle_Factura> listDeatlleFactura) {
+		this.listDeatlleFactura = listDeatlleFactura;
+	}
 
 	/**
 	 * @return the codigo
